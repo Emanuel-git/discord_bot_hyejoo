69b5ad2ba427cd66e9efb36972ae3cccdc2a8ce3
Emanuel <jose.eosousa@gmail.com>
2021-06-29 16:42:19 -0300

    add package.json, get method and activity to Hyejoo

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -4,5 +4,5 @@ module.exports.run = async (client, message, args) => {
     const reason = args.join(" ")
     message.delete().catch(m => {})
     message.channel.send(`> Sendo desligada por motivos de: ${reason}`)
-    client.off()
+    //client.off()
 }
--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const express = require('express')
 const app = express()
 
-// app.get("/", (request, response) => {
-//     const ping = new Date()
-//     ping.setHours(ping.getHours() - 3)
-
-//     console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`)
-//     response.sendStatus(200)
-// })
-
-// app.listen(process.env.PORT) // Recebe solicitações que o deixam online
-
 const Discord = require("discord.js") // Conexão com a biblioteca Discord.js
 const client = new Discord.Client() // Criação de um novo Client
 const config = require("./config.json") // Pegando o token e o prefixo do bot para respostas de comandos
 
-client.on("ready", () => {
+app.listen(config.PORT) // Recebe solicitações que o deixam online
+
+app.get("/", (request, response) => {
+    const ping = new Date()
+    ping.setHours(ping.getHours() - 3)
+
+    console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`)
+    response.sendStatus(200)
+})
+
+client.on('ready', () => {
     console.log('Conectado ao servidor discord')
+    client.user.setActivity('[MV] 이달의 소녀 (LOONA) "PTT (Paint The Town)"', { type: 'WATCHING' })
+})
+
+client.on("guildMemberAdd", member => {
+    
 })
 
 client.on("message", message => {
     if (message.author.bot) return
-    if (message.channel.type == "dm") console.log("dm: " + message.content)
+    if (message.channel.type == "dm") console.log(message.author.username + ": " + message.content)
     if (!message.content.toLowerCase().startsWith(config.prefix)) return
     console.log("message.content: " + message.content)
 
