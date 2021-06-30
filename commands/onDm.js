15a813536081f86b2280c86d1aa9c07b06568ca5
Emanuel <jose.eosousa@gmail.com>
2021-06-30 08:34:48 -0300

    refactoring the 'onDm' command

--- commands/onDm.js
+++ commands/onDm.js
@@ -1,18 +1,10 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-    
-    if (args[0].includes('<@!')) {
-        const personToDm = args.shift().replace('<@!', '').replace('>', '')
-        console.log(personToDm)
-        const messageToDm = args.join(' ')
-        console.log(messageToDm)
-    
-        client.users.cache.get(personToDm).send(messageToDm)
-        message.delete().catch(m => {})
-    }
-    else {
-        message.delete().catch(m => {})
-        message.channel.send("> Não consegui encontrar essa pessoa pra mandar dm")
-    }
+    if (args[0].includes('<@')) args.shift() // '<@' means that the first item is a user mention, so we take it off
+    let user = message.mentions.users.first() || message.author // if there is no mention, the dm is send to who run the command
+    let msgToSend = args.join(' ')
+
+    client.users.cache.get(user.id).send(msgToSend)
+    message.delete().catch(m => {})
 }
